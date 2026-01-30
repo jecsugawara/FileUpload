e48795f06a92233283a3b99ab9bd884f40975822
T.Sugawara <sugawara@jec.ac.jp>
2026-01-30 17:08:21 +0900

    ハッシュ化したパスワードをresponseで表示するように変更

--- src/main/java/PasswordHash.java
+++ src/main/java/PasswordHash.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -12,15 +13,23 @@ import org.apache.commons.codec.digest.DigestUtils;
 public class PasswordHash extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		
 		String plainPassword = "123456789";
 
 		// シンプルなハッシュ
 		String hashed = DigestUtils.sha256Hex(plainPassword);
-		System.out.println("ハッシュ（単純）: " + hashed);
+		System.out.println("SHA256ハッシュ（単純）      : " + hashed);
 
 		// ソルト付きハッシュ（推奨）
 		String salt = "randomSalt123";
 		String saltedHash = DigestUtils.sha256Hex(plainPassword + salt);
-		System.out.println("ハッシュ（ソルト付き）: " + saltedHash);
+		System.out.println("SHA256ハッシュ（ソルト付き）: " + saltedHash);
+		
+		PrintWriter out = response.getWriter();
+		out.println("<p>パスワード: " + plainPassword + "</p>");
+		out.println("<p>SHA256ハッシュ(単純)　　　：" + hashed + "</p>");
+		out.println("<p>SHA256ハッシュ(ソルト付き)：" + saltedHash + "</p>");
+		
 	}
 }
